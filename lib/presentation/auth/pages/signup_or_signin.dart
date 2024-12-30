ffeb687d5dddfc8fdc7832835666e9241b60af14
madhusudanpangarkar <madhusudanpangarkar28@gmail.com>
2024-12-31 01:44:21 +0530

    feat: images added and initial signin-signup page

--- lib/presentation/auth/pages/signup_or_signin.dart
+++ lib/presentation/auth/pages/signup_or_signin.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:spotify/core/configs/assets/app_images.dart';
 import 'package:spotify/core/configs/assets/app_vectors.dart';
 
 class SignupOrSigninPage extends StatelessWidget{
@@ -15,7 +16,48 @@ class SignupOrSigninPage extends StatelessWidget{
             child: SvgPicture.asset(
               AppVectors.topPattern
             ),
-          )
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: SvgPicture.asset(
+              AppVectors.bottomPattern,
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Image.asset(
+              AppImages.authBG,
+            ),
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SvgPicture.asset(
+                  AppVectors.logo
+                ),
+                const SizedBox(height: 55,),
+                const Text(
+                  'Enjoy Listening To Music',
+                  style: const TextStyle(
+                    fontSize: 26,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 21,),
+                const Text(
+                  'Spotify is a proprietary Swedish audio streaming and media service provider',
+                  style: TextStyle(
+                    fontSize: 17,
+                    fontWeight: FontWeight.w500
+                  ),
+                  textAlign: TextAlign.center,
+                )
+              ],
+            ),
+          ),
         ],
       ),
     );
