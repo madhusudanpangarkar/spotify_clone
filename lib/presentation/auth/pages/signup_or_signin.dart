6d57331ba7fa45e1055f0b257e21c6c82ba39e76
madhusudanpangarkar <madhusudanpangarkar28@gmail.com>
2025-01-02 00:29:38 +0530

    feat: signup navigation done

--- lib/presentation/auth/pages/signup_or_signin.dart
+++ lib/presentation/auth/pages/signup_or_signin.dart
@@ -6,6 +6,7 @@ import 'package:spotify/common/widgets/button/basic_app_button.dart';
 import 'package:spotify/core/configs/assets/app_images.dart';
 import 'package:spotify/core/configs/assets/app_vectors.dart';
 import 'package:spotify/core/configs/theme/app_colors.dart';
+import 'package:spotify/presentation/auth/pages/signup.dart';
 
 class SignupOrSigninPage extends StatelessWidget{
   const SignupOrSigninPage({super.key});
@@ -71,7 +72,14 @@ class SignupOrSigninPage extends StatelessWidget{
                       Expanded(
                         flex: 1,
                         child: BasicAppButton(
-                          onPressed: () {},
+                          onPressed: () {
+                            Navigator.push(
+                              context, 
+                              MaterialPageRoute(
+                                builder: (context) => Signup(),
+                              )
+                            );
+                          },
                           title: 'Register',
                         ),
                       ),
